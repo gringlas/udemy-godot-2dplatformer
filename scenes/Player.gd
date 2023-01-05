6a31782beb4cdc9f40267fb52b229e5beebc08d5
Sebastian Köller <mail@sebastian-koeller.de>
2023-01-05 23:48:40 +0100

    finer player movement finished

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,10 +1,11 @@
 extends KinematicBody2D
 
-var gravity = 300
+var gravity = 1000
 var velocity = Vector2.ZERO
-var maxHorizontalSpeed = 100
+var maxHorizontalSpeed = 140
 var horizontalAcceleration = 1500
-var maxJump = 150
+var maxJump = 360
+var jumpTerminationMultiplier = 4
 
 func _ready() -> void:
 	pass # Replace with function body.
@@ -17,13 +18,20 @@ func _process(delta) -> void:
 	
 	velocity.x += moveVector.x * horizontalAcceleration * delta
 	if (moveVector.x == 0):
-		velocity.x = lerp(velocity.x, 0, .1)
-	
+		#velocity.x = lerp(velocity.x, 0, .1)
+		velocity.x = lerp(0, velocity.x, pow(2, -40 * delta))
+		
 	velocity.x = clamp(velocity.x, - maxHorizontalSpeed, maxHorizontalSpeed)
 	
+	# only jump when on floor, no flying by pressing again
 	if (moveVector.y < 0 && is_on_floor()):
 		velocity.y = moveVector.y * maxJump
 	
-	velocity.y += gravity * delta
+	# jump longer presssing makes you jump higher 
+	if (velocity.y < 0 && !Input.is_action_pressed("jump")):
+		velocity.y += gravity * jumpTerminationMultiplier * delta
+	else:
+		velocity.y += gravity * delta
+	
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
