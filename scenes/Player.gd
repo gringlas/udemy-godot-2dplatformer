6e3f6c09fbe0b198515a9e3269f802cfed03b16e
Sebastian Köller <mail@sebastian-koeller.de>
2023-01-05 23:25:17 +0100

    finer player movement

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 var gravity = 300
 var velocity = Vector2.ZERO
 var maxHorizontalSpeed = 100
+var horizontalAcceleration = 1500
 var maxJump = 150
 
 func _ready() -> void:
@@ -14,7 +15,11 @@ func _process(delta) -> void:
 	moveVector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	moveVector.y = -1 if Input.is_action_just_pressed("jump") else 0
 	
-	velocity.x = moveVector.x * maxHorizontalSpeed
+	velocity.x += moveVector.x * horizontalAcceleration * delta
+	if (moveVector.x == 0):
+		velocity.x = lerp(velocity.x, 0, .1)
+	
+	velocity.x = clamp(velocity.x, - maxHorizontalSpeed, maxHorizontalSpeed)
 	
 	if (moveVector.y < 0 && is_on_floor()):
 		velocity.y = moveVector.y * maxJump
