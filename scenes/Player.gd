c86f8388977066b9a2520a26009a708552165509
Sebastian Köller <mail@sebastian-koeller.de>
2023-01-23 00:34:14 +0100

    spikes hazard

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+signal died
+
 var gravity = 1000
 var velocity = Vector2.ZERO
 var maxHorizontalSpeed = 140
@@ -9,7 +11,7 @@ var jumpTerminationMultiplier = 4
 var hasDoubleJump = false
 
 func _ready() -> void:
-	pass # Replace with function body.
+	$HazardArea.connect("area_entered", self, "on_hazard_area_entered")
 
 
 func _process(delta) -> void:
@@ -64,3 +66,6 @@ func update_animation():
 	
 	if (moveVector.x != 0):
 		$AnimatedSprite.flip_h = true if moveVector.x > 0 else false
+
+func on_hazard_area_entered(area2d):
+	emit_signal("died")
