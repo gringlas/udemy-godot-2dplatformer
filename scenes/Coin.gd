b68f982bd92bd7a099ac2fbf3a96a14796816043
Sebastian Köller <mail@sebastian-koeller.de>
2023-01-30 09:10:20 +0100

    coin collecting logic

--- scenes/Coin.gd
+++ scenes/Coin.gd
@@ -1,13 +1,18 @@
 extends Node2D
 
+var baseLevel = null
+
 func _ready():
 	$Area2D.connect("area_entered", self, "on_area_entered")
 	$AnimationPlayer.play("idle")
+	baseLevel = get_tree().get_nodes_in_group("base_level")[0]
 	
 func on_area_entered(area2d):
 	if (area2d.is_in_group("Player")):
 		$AnimationPlayer.play("pickup")
 		call_deferred("disable_pickup")
+		#var baseLevel = get_tree().get_nodes_in_group("base_level")[0]
+		baseLevel.coin_collected()
 	
 func disable_pickup():
 	$Area2D/CollisionShape2D.disabled = true
