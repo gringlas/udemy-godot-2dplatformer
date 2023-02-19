98c6e8d1bf449a1dcc3663d981f8cb0b49ed61e8
Sebastian Köller <mail@sebastian-koeller.de>
2023-02-19 01:08:08 +0100

    mouse cursor

---
+++ scenes/UI/MouseCursor.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	pass
+	
+
+func _process(delta: float) -> void:
+	$CursorSprite.global_position = $CursorSprite.get_global_mouse_position()
+	if Input.is_action_pressed("click"):
+		$AnimationPlayer.play("click")
+	
+	
